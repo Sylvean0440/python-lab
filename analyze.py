63b588870bf9645f65895e297e9bb055edb62cd6
Sylvean0440 <luoguochao30aabb@163.com>
2026-09-22 15:31:31 +0800

    第二次修改：增加标题统计功能

--- analyze.py
+++ analyze.py
@@ -8,9 +8,12 @@ lines = f.readlines()
 print('行数为', len(lines))
 words = ''.join(lines)
 print('字数为', len(words))
+count=0
 for line in lines:
     if line.startswith('#'):
         print(line)
+        count= count + 1
+print(f'一共有{count}个标题')
 result = f'行数为{len(lines)}\n字数为{len(words)}\n'
 out = open('分析结果.txt', 'w', encoding='utf-8')
 out.write(result)
